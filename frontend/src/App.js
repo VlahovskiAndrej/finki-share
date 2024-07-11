c63f211b1482d338a85c9f35a74e6d62bee96ce1
vladimir028 <107487074+vladimir028@users.noreply.github.com>
2024-07-11 14:00:53 +0200

    front end design changed added new pages and new components added new public folders for the avatars, students organizations and additional images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,31 +4,64 @@ import NavigationBar from "./components/NavigationBar";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import HomePage from './pagesNew/HomePage';
 import CardComponent from "./components/CardComponent";
 import {Col, Row} from "react-bootstrap";
 import SelectPage from "./pages/SelectPage";
 import MaterialsPage from "./pages/MaterialsPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import ChooseSubject from "./pagesNew/ChooseSubject";
+import AllSubjects from "./pagesNew/AllSubjects";
+
+
+// function App() {
+//   return (
+//
+//     <div className="Main">
+//
+//         <Router>
+//             <Routes>
+//                 <Route path="/" element={<HomePage />} />
+//                 <Route path="/select" element={<SelectPage />} />
+//                 <Route path="/materials" element={<MaterialsPage />} />
+//                 <Route path="/login" element={<LoginPage />} />
+//                 <Route path="/register" element={<RegisterPage />} />
+//                 {/* Other routes */}
+//             </Routes>
+//         </Router>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
 
 function App() {
-  return (
+    return (
 
-    <div className="Main">
+        <div className="Main">
 
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/select" element={<SelectPage />} />
-                <Route path="/materials" element={<MaterialsPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                {/* Other routes */}
-            </Routes>
-        </Router>
-    </div>
-  );
+
+
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    {/* <Route path="/select" element={<SelectPage />} /> */}
+                    <Route path="/materials" element={<MaterialsPage />} />
+                    {/*<Route path="/test" element={< TestPage/>} />*/}
+
+                    <Route path="/homepage" element={< HomePage/>} />
+                    <Route path="/select" element={<ChooseSubject />} />
+                    <Route path="/select/subjectsAll" element={<AllSubjects />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    {/* Other routes */}
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
+
---
+++ frontend/src/componentsNew/Navigation/Navigation.js
@@ -0,0 +1,89 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {useNavigate} from 'react-router-dom';
+import React from 'react'
+import {Navbar, Nav} from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from './Navigation.module.css'
+import useAuthStatus from "../../hooks/useAuthStatus";
+
+
+export default function Navigation() {
+
+    const {isLoggedIn, username} = useAuthStatus();
+
+    let navigate = useNavigate();
+
+    function routeChange() {
+        let path = '/';
+        navigate(path);
+    }
+
+    function MouseOver(event) {
+        event.target.style.color = 'white';
+        event.target.style.backgroundColor = '#814A35';
+        event.target.style.borderRadius = '40em';
+    }
+
+    function MouseOut(event) {
+        event.target.style.color = 'black';
+        event.target.style.backgroundColor = 'white';
+    }
+
+    return (
+        <Navbar expand="lg">
+            <Container>
+
+                <img src='/images/finkilogo.png ' style={{width: '5%', height: '5%'}} onClick={routeChange}/>
+                <Navbar.Brand href="/" style={{color: 'white'}}>
+
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav style={{marginInlineStart: 'auto'}}>
+                        <Nav.Link
+                            className={styles.custombutton}
+                            href="/select"
+                            onMouseOver={MouseOver}
+                            onMouseOut={MouseOut}
+                        >
+                            Избери предмет
+                        </Nav.Link>
+
+                        <Nav.Link href="/materials"
+                                  className={styles.custombutton}
+                                  onMouseOver={MouseOver}
+                                  onMouseOut={MouseOut}
+                        >
+                            Материјали
+                        </Nav.Link>
+                        <Nav.Link href="/register"
+                                  className={styles.custombutton}
+                                  onMouseOver={MouseOver}
+                                  onMouseOut={MouseOut}
+                        >
+                            Регистрирај се
+                        </Nav.Link>
+
+                        {/*/!*<Nav.Link href="/login"*!/*/}
+                        {/*/!*          className={styles.custombutton}*!/*/}
+                        {/*/!*          onMouseOver={MouseOver}*!/*/}
+                        {/*/!*          onMouseOut={MouseOut}*!/*/}
+                        {/*/!*>*!/*/}
+                        {/*/!*    Најави се*!/*/}
+                        {/*</Nav.Link>*/}
+
+                        {isLoggedIn ?
+                            (<Nav.Link href="http://localhost:8080/logout" style={{color: 'red'}}>{username} - Logout</Nav.Link>) :
+                            (<Nav.Link href="http://localhost:8080/login" style={{color: 'black'}}>Најави се</Nav.Link>)
+                        }
+
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
